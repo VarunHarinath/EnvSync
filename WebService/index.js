1ff8aaffadac2a3ad44ff943fc0bb93c0994a59a
Varun Harinath <varunharinath15@gmail.com>
2026-01-28 17:42:17 -0600

    Clerk Auth Test Setup

--- WebService/index.js
+++ WebService/index.js
@@ -1,16 +1,31 @@
+// Module imports...
 import express from "express";
 import cors from "cors";
 import * as Sentry from "@sentry/node";
+import {
+  clerkMiddleware,
+  requireAuth,
+  getAuth,
+  clerkClient,
+} from "@clerk/express";
+import { configDotenv } from "dotenv";
+
+// Components imports...
 import { homeRouter } from "./routes/HomeRoute.js";
 import { projectRouter } from "./routes/ProjectRoute.js";
 import { environmentRouter } from "./routes/EnvironmentRoute.js";
 import { secretRouter } from "./routes/SecretRoute.js";
 import { environmentSecretoute } from "./routes/EnvironmentSecretRoute.js";
 
+// Dotenv Config
+configDotenv();
+
+// Serive Instance
 const webService = express();
 
 webService.use(express.json());
 webService.use(cors());
+webService.use(clerkMiddleware());
 
 // Routes/controllers first
 webService.use("/api/v1/environment_secret", environmentSecretoute);
@@ -18,6 +33,17 @@ webService.use("/api/v1/secret", secretRouter);
 webService.use("/api/v1/environment", environmentRouter);
 webService.use("/api/v1", homeRouter);
 webService.use("/api/v1/project", projectRouter);
+
+// Test Route For Clerk
+webService.get("/user", requireAuth(), async (req, res) => {
+  try {
+    const { userId } = getAuth(req);
+    const user = await clerkClient.users.getUser(userId);
+    res.json(user);
+  } catch (e) {
+    console.error(e);
+  }
+});
 
 // Debug route must be before error handlers
 webService.get("/debug-sentry", (req, res) => {
